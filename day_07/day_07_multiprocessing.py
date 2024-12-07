7a4d6d4e4201337bd35a634ac829a1f5fd070350
Max Pare <maxparegd@gmail.com>
2024-12-07 20:09:48 +0100

    Added a break that makes the program 0.05% more efficient

--- day_07/day_07_multiprocessing.py
+++ day_07/day_07_multiprocessing.py
@@ -3,16 +3,7 @@ import multiprocessing
 from time import sleep
 import random
 from numpy import array_split
-testinput = '190: 10 19\n'\
-'3267: 81 40 27\n'\
-'83: 17 5\n'\
-'156: 15 6\n'\
-'7290: 6 8 6 15\n'\
-'161011: 16 10 13\n'\
-'192: 17 8 14\n'\
-'21037: 9 7 18 13\n'\
-'292: 11 6 16 20'
-_raw_data = testinput.split('\n')
+
 with open('./input.txt', 'r') as file:
 	_raw_data = file.read().split('\n')
 OPS = ['*', '+', '||']
@@ -41,7 +32,6 @@ def worker(data, _collector):
 				cur.append(num)
 				if j < len(combo): cur.append(combo[j])
 			operations.append(cur)
-		#print(f'Starting equation {eq_ind} out of {len(data)}')
 		counter = 0
 		for i, operation in enumerate(operations):
 			counter += 1
@@ -73,6 +63,7 @@ def worker(data, _collector):
 					total_valid_elephant[(hash(''.join([val for val in operation if try_int(val)])))] = total
 				else:
 					total_valid_regular[(hash(''.join([val for val in operation if try_int(val)])))] = total
+				break
 	_collector.put(
 		sum([val for val in total_valid_regular.values()]) + sum([val for val in total_valid_elephant.values()]))
 	print('Done')
@@ -93,30 +84,5 @@ def main(_raw_data_):
 		sleep(.1)
 	print(sum(results))
 
-
 if __name__ == '__main__':
 	main(_raw_data)
-
-'''
-for equation_raw in _raw_data[1:2]: # eval() fucking sucks, it interprets '121+27' as '121 * 2 + 7' fuck you, you fucking assholes
-	target = equation_raw.split(':')[0]
-	numbers = equation_raw.split(':')[1].split()
-	combos = [item for item in itertools.product(operators, repeat=len(numbers) - 1)]
-	operations = []
-	for combo in combos:
-		current = []
-		for i, num in enumerate(numbers):
-			current.append(num)
-			if i < len(combo): current.append(combo[i])
-		operations.append(current)
-	for operation in operations:
-		print(operation)
-		_t = ''.join(operation[:3])
-		print(_t)
-		current_total = eval(_t)
-		for i in range(len(operation))[3::2]:
-			to_eval = f'{current_total}{operation[i]}{operation[i + 1]}'
-			print(to_eval)
-			current_total += eval(to_eval)
-			print(current_total)
-'''
