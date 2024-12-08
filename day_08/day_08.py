c7b421b42c6669845ef50b780d572f183363c5ce
Max Pare <maxparegd@gmail.com>
2024-12-09 00:35:07 +0100

    day 08 done!

--- day_08/day_08.py
+++ day_08/day_08.py
@@ -1,17 +1,7 @@
 import math
 
-grid_raw = '............\n' \
-           '........0...\n' \
-           '.....0......\n' \
-           '.......0....\n' \
-           '....0.......\n' \
-           '......A.....\n' \
-           '............\n' \
-           '............\n' \
-           '........A...\n' \
-           '.........A..\n' \
-           '............\n' \
-           '............'.split('\n')
+with open('input.txt','r') as file:
+	grid_raw = file.read().split('\n')
 MAX_X, MAX_Y = len(grid_raw[0]) - 1, len(grid_raw) - 1
 grid_d = {(x, y): grid_raw[y][x] for y in range(len(grid_raw)) for x in range(len(grid_raw[0]))}
 
@@ -41,10 +31,8 @@ class Vector2:
 		return v1[0] - v2[0], v1[1] - v2[1]
 
 	@staticmethod
-	def normalize(v):
-		if v[0] == v[1]: return 1 * get_sign(v[0]), 1  * get_sign(v[1])
-		if v[0] < v[1]: pass
-		return None
+	def flip_sign(v1):
+		return v1[0] * (get_sign(v1[0]) * -1), v1[1] * (get_sign(v1[1]) * -1)
 
 	@staticmethod
 	def distance(v1:tuple[int,int], v2:tuple[int,int]):
@@ -60,55 +48,49 @@ class Vector2:
 		return abs(v[0]), abs(v[1])
 
 	@staticmethod
-	def in_bounds(v, max_x, max_y):
-		return not(v[0] > max_x or v[0] < 0)
-	@staticmethod
-	def points_on_line(v1:tuple[int,int], v2:tuple[int,int], max_x=MAX_X, max_y=MAX_Y):
-		points = set()
-		_offset = Vector2.normalize(Vector2.abs(Vector2.sub(v1,v2)))
-		points.add(v1)
-		_current_pos = v1
-		flip = False
-		while True:
-			points.add(_current_pos)
-			_current_pos = Vector2.add(_current_pos, _offset)
-			if (_current_pos[0] > max_x or _current_pos[0] < 0) or (_current_pos[1] > max_y or _current_pos[1] < 0):
-				if flip: break
-				_current_pos = v1
-				_offset = Vector2.negate(_offset)
-				flip = True
-		return list(points)
+	def in_bounds(v, max_x=MAX_X, max_y=MAX_Y):
+		return not(v[0] > max_x or v[0] < 0 or v[1] < 0 or v[1] > max_y)
 
 	@staticmethod
-	def put_antinodes(v1:tuple[int,int], v2:tuple[int,int], max_x=MAX_X, max_y=MAX_Y):
-		_offset = Vector2.normalize(Vector2.sub(v1,v2))
-		points = [Vector2.add(v1, _offset), Vector2.add(v1, Vector2.negate(_offset)),
-		          Vector2.add(v2, _offset), Vector2.add(v2, Vector2.negate(_offset))]
-		return [p for p in points if p and all(not c_ < 0 for c_ in p) and p != v1 and p != v2]
+	def put_antinodes(v1:tuple[int,int], v2:tuple[int,int], max_x=MAX_X, max_y=MAX_Y, infinite=False):
+		start_offset = Vector2.sub(v1,v2)
+		_offset = start_offset
+		if infinite and _offset[0] == _offset[1]: _offset = (1,1)
+		results = []
+		counter = 0
+		while True:
+			_current = [Vector2.add(v1, _offset), Vector2.add(v1, Vector2.negate(_offset)),
+			          Vector2.add(v2, _offset), Vector2.add(v2, Vector2.negate(_offset))]
+			if all(not Vector2.in_bounds(_elem) for _elem in _current): break # probably makes infinite loop in some cases, too bad!
+			results += _current
+			if not infinite: break
+			_offset = Vector2.add(_offset, start_offset)
+		return [p for p in set(results)
+		        if p and Vector2.in_bounds(p, max_x,max_y)
+		        and (p != v1 or infinite)
+		        and (p != v2 or  infinite)]
 
 	@staticmethod
 	def direction(v1, v2):
 		return Vector2.add(v1, v2)
 
-print(Vector2.points_on_line((2,0),(0,7)))
-exit()
+
 antinodes = []
-for point0, char0 in grid_d.items():
+antennas = set()
+for p0, char0 in grid_d.items():
 	if char0 == '.':
 		continue
-	for point1, char1 in grid_d.items():
-		if (point0 == point1) or (char1 == '.') or (char0 != char1): continue
-		print(point0, point1)
-		print(char0, char1)
-		a_nodes = Vector2.put_antinodes(point0, point1)
-		print(a_nodes)
+	for p1, char1 in grid_d.items():
+		if (p0 == p1) or (char1 in '.#') or (char0 != char1): continue
+		antennas.add(p0)
+		antennas.add(p1)
+		a_nodes = Vector2.put_antinodes(p0, p1, infinite=True)
 		antinodes += a_nodes
-		print()
+
 
 antinodes = set(antinodes)
-print(antinodes)
-print(len(antinodes))
-_visual = [[grid_raw[y][x]  for x in range(len(grid_raw[0]))]for y in range(len(grid_raw))]
+print(len(antinodes), 'antinodes')
+_visual = [[grid_raw[y][x] for x in range(len(grid_raw[0]))] for y in range(len(grid_raw))]
 for x,y in antinodes:
 	_visual[y][x] = '#'
 with open('./visual.txt','w') as file:
